b9789e199d35c5409b319ce76193c9c669aba44c
Cicero Antonio Ferreria Ferreira <ciceroferreira2010@hotmail.com>
2021-06-25 22:39:20 -0300

    iniciando novo projeto

---
+++ WebBarraProgresso/wwwroot/js/barra.js
@@ -0,0 +1,18 @@
+﻿var barraProgress = new Object();
+barraProgress.porcentagem = 25;
+barraProgress.carregaBarra = function () {
+
+    if (barraProgress.porcentagem > 100)
+        barraProgress.porcentagem = 0;
+    $(".barra-progress").css("width", barraProgress.porcentagem + "%");
+
+    $(".porcentagem-barra-progress").html(barraProgress.porcentagem + "%");
+
+
+    barraProgress.porcentagem += 5;
+    setTimeout(barraProgress.carregaBarra, 150);
+}
+
+$(function () {
+    barraProgress.carregaBarra();
+})
